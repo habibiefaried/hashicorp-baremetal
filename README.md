f3c85292096045ece79bb144bb6fb6ca42461b4e
Habibie Faried <habibiefaried@gmail.com>
2020-11-29 12:18:59 +0700

    OVS guide

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # hashicorp-baremetal
 Hashicorp stack on baremetal deployment
+
+# Spec
+* 3 VPS S SSD servers on contabo.com (public IP only)
+* We are using 192.168.1.0/24 for our private networking. 1.1, 1.2, 1.3 is being used for each of those hosts respectively.
+* IPSec PSK: `swordF1sh`
+
+# OpenVSwitch Commands
+* Add bridge: `ovs-vsctl add-br <bridge name>`
+* Set IP on bridge: `ip addr add <IP>/<subnet number> dev <bridge name>`
+* Up interface: `ip link set <bridge name> up`
+* Add port: `ovs-vsctl add-port <bridge name> <port name>`
+* Delete port: ` ovs-vsctl del-port <bridge name> <port name>`
+* List ports on bridge: `ovs-vsctl list-ports <bridge name>`
+* Set interface for IPSec: `ovs-vsctl set interface <port name> type=gre options:remote_ip=<REMOTE IP> options:psk=<PASSWORD>`
+* OVS status: `/usr/share/openvswitch/scripts/ovs-ctl status`
+* IPSec tunnel status: `ovs-appctl -t ovs-monitor-ipsec tunnels/show`
+* Delete IP on bridge: `ip addr del <IP>/<subnet number> dev <bridge name>`
+* Delete bridge: `ovs-vsctl del-br <bridge name>`
+
+# Reference
+* https://docs.openvswitch.org/en/latest/tutorials/ipsec/
--- openvswitch_install.sh
+++ openvswitch_install.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-apt update 
-apt install dkms strongswan build-essential fakeroot -y # Build
-wget https://www.openvswitch.org/releases/openvswitch-2.14.0.tar.gz
-tar -xvf openvswitch-2.14.0.tar.gz
-apt install graphviz autoconf automake1.10 bzip2 debhelper dh-autoreconf libssl-dev libtool openssl procps python3-all python3-sphinx python3-twisted python3-zope.interface libunbound-dev libunwind-dev -y # Build Depends on this version
-apt install iproute2 python3-openvswitch -y # For openvswitch
-cd openvswitch-2.14.0
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+VERSION=2.14.0
+echo "Installing dependencies..."
+apt update
+apt install git dkms strongswan build-essential fakeroot iproute2 -y # Common build
+apt install graphviz autoconf automake1.10 bzip2 debhelper dh-autoreconf libssl-dev libtool openssl procps python3-all python3-sphinx python3-twisted python3-zope.interface libunbound-dev libunwind-dev -y # Build depends on this version
+echo "Downloading source..."
+# git clone https://github.com/openvswitch/ovs && cd ovs && git checkout v2.14.0
+wget https://www.openvswitch.org/releases/openvswitch-$VERSION.tar.gz && tar -xvf openvswitch-$VERSION.tar.gz && cd openvswitch-$VERSION
+echo "Check and compiling..."
 dpkg-checkbuilddeps
 automake --add-missing
 DEB_BUILD_OPTIONS='parallel=8' fakeroot debian/rules binary
+cd ..
+ls -lah | grep .deb
+echo "Installing..."
+dpkg -i *.deb || true
+apt install -f -y
+/usr/share/openvswitch/scripts/ovs-ctl start
+/usr/share/openvswitch/scripts/ovs-ctl status
+sysctl -w net.ipv4.ip_forward=1 # Make this permanent by edit /etc/sysctl.conf

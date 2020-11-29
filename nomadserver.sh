2b86e1acabe8e53dcb9b0ffc658ba3749b138299
Habibie Faried <habibiefaried@gmail.com>
2020-11-29 18:11:28 +0700

    Change to ubuntu

--- nomadserver.sh
+++ nomadserver.sh
@@ -25,7 +25,7 @@ echo "Installing docker..."
 apt update
 apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" -y
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 apt update
 apt install docker-ce docker-ce-cli containerd.io -y
 docker run hello-world

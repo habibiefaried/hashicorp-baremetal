42a9e1b6e18bf6a764736f9a4558cc1b8e662063
Habibie Faried <habibiefaried@gmail.com>
2020-11-29 19:11:13 +0700

    change to 2.13

--- openvswitch_install.sh
+++ openvswitch_install.sh
@@ -3,10 +3,10 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-VERSION=2.14.0
+VERSION=2.13.0
 echo "Installing dependencies..."
 apt update
-apt install dkms strongswan build-essential fakeroot iproute2 -y # Common build
+apt install python3-pip dkms strongswan build-essential fakeroot iproute2 -y # Common build
 apt install graphviz autoconf automake1.10 bzip2 debhelper dh-autoreconf libssl-dev libtool openssl procps python3-all python3-sphinx python3-twisted python3-zope.interface libunbound-dev libunwind-dev -y # Build depends on this version
 echo "Downloading source..."
 wget https://www.openvswitch.org/releases/openvswitch-$VERSION.tar.gz && tar -xvf openvswitch-$VERSION.tar.gz && cd openvswitch-$VERSION
@@ -17,6 +17,7 @@ DEB_BUILD_OPTIONS='parallel=8' fakeroot debian/rules binary
 cd ..
 ls -lah | grep .deb
 echo "Installing..."
+pip3 install ovs
 dpkg -i *.deb || true
 apt install -f -y
 /usr/share/openvswitch/scripts/ovs-ctl start

519a4719dd6ac421df9e749080c56ae4201cf675
Habibie Faried <habibiefaried@gmail.com>
2020-11-30 20:30:35 +0700

    Removing docker from nomad server installation

--- nomadserver.sh
+++ nomadserver.sh
@@ -21,17 +21,6 @@ if [ -z "${CONSUL_SERVER_IP}" ]
   exit
 fi
 apt update && apt install curl unzip -y
-echo "Installing docker..."
-apt update
-apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" -y
-apt update
-apt install docker-ce docker-ce-cli containerd.io -y
-echo '{ "dns" : [ "172.17.0.1" , "172.31.0.2" ] }'  > /etc/docker/daemon.json
-systemctl enable docker
-service docker reload
-docker run hello-world
 echo "Installing Nomad..."
 cd /tmp/
 curl -sSL https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip -o nomad.zip

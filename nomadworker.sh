eb9198353d883fe5249415589cd020a2816688b5
Habibie Faried <habibiefaried@gmail.com>
2020-12-05 09:35:30 +0700

    add cni plugin

--- nomadworker.sh
+++ nomadworker.sh
@@ -141,3 +141,6 @@ WantedBy=multi-user.target
 EOF
 systemctl enable consul
 systemctl start consul
+curl -L -o cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v0.8.7/cni-plugins-linux-amd64-v0.8.7.tgz
+mkdir -p /opt/cni/bin
+tar -C /opt/cni/bin -xzf cni-plugins.tgz
